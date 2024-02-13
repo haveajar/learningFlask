e65ad3b40c4040bc354af97faf1cf8833ef823c7
colin <bfz@DESKTOP-SMFFBPP>
2024-02-13 14:58:37 +0100

    Finished chapter 4

--- microblog.py
+++ microblog.py
@@ -1,4 +1,12 @@
-from app import app
+import sqlalchemy as sqla
+import sqlalchemy.orm as sqlorm
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sqla': sqla, 'sqlorm': sqlorm, 'db': db, 'User': User, 'Post': Post}
 
 
 if __name__ == "__main__":

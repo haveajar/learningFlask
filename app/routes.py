4bcb7ae9139fa815711f6b690affb2d014bd65ff
colin <bfz@DESKTOP-SMFFBPP>
2024-02-14 11:03:52 +0100

    Continued work on login

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sqla
 from app import db
 from app.models import User
+from urllib.parse import urlsplit
 
 
 @app.route('/')
@@ -27,7 +28,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 # This does not work :(
@@ -48,7 +49,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 

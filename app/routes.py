8558e2d469453bbbaa758d29e7d586cc1f5c03b4
colin <bfz@DESKTOP-SMFFBPP>
2024-02-14 13:56:43 +0100

    Added registration of new users

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sqla
 from app import db
 from app.models import User
+from app.forms import RegistrationForm
 from urllib.parse import urlsplit
 
 
@@ -35,6 +36,21 @@ def index():
 @app.route('/ragnar')
 def ragnar():
     return "Ragnar is inherently underpwered."
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])

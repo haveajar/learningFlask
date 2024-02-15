dfd91365ff03d25a743a3d4e2dfa71263aee2c47
colin <bfz@DESKTOP-SMFFBPP>
2024-02-15 09:47:04 +0100

    Can now edit user profile info

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, length
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 import sqlalchemy as sqla
 from app import db
 from app.models import User
